92878f4ccc1453a571da0331877a7050e1fbf1f7
Stéphane Dauguet <sdauguet@gmail.com>
2022-11-15 15:42:20 +0100

    crud for user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -6,8 +6,50 @@ import { User } from './user.entity';
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
     return this.repo.save(user);
+  }
+
+  async findOne(id: number) {
+    const user = await this.repo.findOne({
+      where: {
+        id,
+      },
+    });
+    return user;
+  }
+
+  async find(email: string) {
+    const users = await this.repo.find({
+      where: { email },
+    });
+    return users;
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.repo.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    const updatedUser = { ...user, ...attrs };
+    return this.repo.save(updatedUser);
+  }
+
+  async remove(id: number) {
+    const user = await this.repo.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return this.repo.remove(user);
   }
 }
